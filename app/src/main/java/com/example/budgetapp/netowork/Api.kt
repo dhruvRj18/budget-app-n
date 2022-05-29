f94a6fbfe79e72616d63748f746389471dd9ae38
dhruvRj18 <dhruv4218@gmail.com>
2022-05-29 16:59:15 +0100

    feat: User Entry in database

--- app/src/main/java/com/example/budgetapp/netowork/Api.kt
+++ app/src/main/java/com/example/budgetapp/netowork/Api.kt
@@ -1,6 +1,16 @@
 package com.example.budgetapp.netowork
 
+
+import com.example.budgetapp.entities.Budget
+import com.example.budgetapp.entities.Profile
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface Api {
 
 
+    @POST("user")
+    suspend fun insertProfileData(
+        @Body profile: Profile
+    ): Budget
 }
--- app/src/main/java/com/example/budgetapp/repositories/ProfileRepository.kt
+++ app/src/main/java/com/example/budgetapp/repositories/ProfileRepository.kt
@@ -2,11 +2,12 @@ package com.example.budgetapp.repositories
 
 import com.example.budgetapp.db.ProfileDao
 import com.example.budgetapp.entities.Profile
+import com.example.budgetapp.netowork.Api
 import kotlinx.coroutines.flow.flow
 import java.util.concurrent.Flow
 import javax.inject.Inject
 
-class ProfileRepository @Inject constructor(val profileDao: ProfileDao) {
+class ProfileRepository @Inject constructor(val profileDao: ProfileDao, val api: Api) {
 
 
     suspend fun insertProfileData(profile: Profile) = profileDao.insertProfileData(profile)
@@ -14,5 +15,7 @@ class ProfileRepository @Inject constructor(val profileDao: ProfileDao) {
      fun getProfile() = profileDao.getProfileData()
 
     suspend fun updateCurrentBalance(revidesBalance:Float) = profileDao.updateCurrentBalance(revidesBalance)
+
+    suspend fun insertProfileDataToApi(profile: Profile) = api.insertProfileData(profile)
 
 }
